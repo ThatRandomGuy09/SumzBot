e7d774b0b2785c7e42d9b9e7a3a363f555e5508b
Vansh Vasishtha <vansh19092002@gmail.com>
2024-11-15 17:38:15 +0530

    Added the Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./services/store";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
+      <Analytics />
       <App />
     </Provider>
   </React.StrictMode>
